fbf51379fb9ab666c54c02b7f3dfa80210c42008
Anderson A Lopes <romalopes@yahoo.com.br>
2026-06-21 16:27:26 +1000

    comment

--- src/services/api.js
+++ src/services/api.js
@@ -21,6 +21,7 @@ async function request(
     ...headers,
   };
 
+  console.log("VITE_API_BASE_URL", import.meta.env.VITE_API_BASE_URL);
   console.log(method, body, auth, headers);
   let payload;
   if (body !== undefined) {
